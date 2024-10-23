eae99cc33d6b8116c910cdba037fe73b81bb5279
Valery Lourie <valerylourie@gmail.com>
2024-10-23 19:08:10 +0300

    Multilingual content

--- web/modules/custom/zz_card/src/Controller/CardController.php
+++ web/modules/custom/zz_card/src/Controller/CardController.php
@@ -2,7 +2,9 @@
 
 namespace Drupal\zz_card\Controller;
 
+use Drupal\ai_translate\TextTranslatorInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\zz_card\HoroscopeGenerator;
 use Drupal\zz_card\Sign;
@@ -14,13 +16,18 @@ class CardController extends ControllerBase {
   use StringTranslationTrait;
 
   public function __construct(
-    protected HoroscopeGenerator $horoscopeGenerator) { }
+    protected HoroscopeGenerator $horoscopeGenerator,
+    protected TextTranslatorInterface $textTranslator,
+  ) {}
 
   /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container) {
-    return new static($container->get('zz_card.generator'));
+    return new static(
+      $container->get('zz_card.generator'),
+      $container->get('ai_translate.text_translator'),
+    );
   }
 
   /**
@@ -41,6 +48,10 @@ class CardController extends ControllerBase {
     $card = \Drupal::entityTypeManager()->getStorage('zz_card')
       ->loadBySignAndDate($real->value, (int) $date);
     if (!$card) {
+      if ($date > 0 && $date < date('Ymd')) {
+        // Cannot generate horoscopes for the past dates.
+        throw new NotFoundHttpException();
+      }
       $content = $this->horoscopeGenerator->generateHoroscope($real);
       $cardStorage = $this->entityTypeManager()
         ->getStorage('zz_card');
@@ -51,6 +62,19 @@ class CardController extends ControllerBase {
       ]);
       $cardStorage->save($card);
     }
+    $content = $card->get('content')->value;
+    $currentLanguage = $this->languageManager()->getCurrentLanguage();
+    if ($card->hasTranslation($currentLanguage->getId())) {
+      $card = $card->getTranslation($currentLanguage->getId());
+    }
+    else {
+      $card = $card->addTranslation($currentLanguage->getId());
+      $card->set('content', $this->textTranslator->translateContent(
+        $content, $currentLanguage, $this->languageManager()->getDefaultLanguage()
+      ));
+      $card->save();
+    }
+
     return [
       '#cache' => [
         'tags' => $card->getCacheTags(),
